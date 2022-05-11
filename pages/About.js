030845538f1a3f122bce48ebe9883be2f95c9710
Daryl Ruggier <daryl.ruggier@gmail.com>
2022-05-11 21:33:25 +0200

    Fixed apostrophe build issue with &apos;

--- pages/About.js
+++ pages/About.js
@@ -8,16 +8,16 @@ const About = () => {
         <h1 id="about">About</h1>
       </div>
       <div className={styles.body}>
-        <h1>Hi &#128075; I'm Daryl!</h1>
+        <h1>Hi &#128075; I&apos;m Daryl!</h1>
         <h2>
-          I'm a Frontend Developer and Computer Science Student at the
+          I&apos;m a Frontend Developer and Computer Science Student at the
           University of Malta. In my free time I enjoy learning about anything
           and everything Web Dev, eSports and Powerlifting &#127947;
         </h2>
         <h2>
-          This website is one of the many projects I've worked on in order to
-          gain further knowledge in the Frontend Development sphere. You can
-          find more information about the projects I've worked on{" "}
+          This website is one of the many projects I&apos;ve worked on in order
+          to gain further knowledge in the Frontend Development sphere. You can
+          find more information about the projects I&apos;ve worked on{" "}
           <a
             href="https://github.com/darylruggier/"
             target="_blank"
